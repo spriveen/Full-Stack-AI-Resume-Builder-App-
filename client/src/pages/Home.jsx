c8713599c4ac11d42f75033036b9251f93869b1d
spriveen <rpeanuka@gmail.com>
2026-01-13 06:56:48 -0800

    update home and featured section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Banner from '../components/home/Banner'
+import Hero from '../components/home/Hero'
+import Features from '../components/home/Features'
 
 const Home = () => {
   return (
     <div>
-      <h1>Home page</h1>
+      <Banner />
+      <Hero />
+      <Features />
     </div>
   )
 }
