33030656fb9f2f39ca8a6e56debf80a17078b6da
spriveen <rpeanuka@gmail.com>
2026-01-16 21:30:27 -0800

    update personalInfoForm.jsx

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { dummyResumeData } from '../assets/assets'
 import { ArrowLeftIcon, Briefcase,  ChevronLeft,  ChevronRight,  FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react'
+import PersonalInfoForm from '../components/PersonalInfoForm'
 
 const ResumeBuilder = () => {
 
@@ -90,6 +91,13 @@ const ResumeBuilder = () => {
                 </button>
             </div>
 
+           </div>
+             {/* form content */}
+           <div className='space-y-6'>
+              {activeSection.id === 'personal' && (
+                <PersonalInfoForm data={resumeData.personal_info} onChange={(data)=>setResumeData(prev =>({...prev, personal_info: data}))} removeBackground={removeBackground}
+                setRemoveBackground={setRemoveBackground} />
+              )}
            </div>
 
           </div>
