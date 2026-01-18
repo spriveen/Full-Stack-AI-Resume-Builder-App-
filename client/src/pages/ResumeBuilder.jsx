94386aabe1dd3a4af9423896b74763a41dacc509
spriveen <rpeanuka@gmail.com>
2026-01-18 08:06:04 -0800

    update educationform.jsx

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -10,6 +10,7 @@ import TemplateSelector from '../components/TemplateSelector'
 import ColorPicker from '../components/ColorPicker'
 import ProfessionalSummaryForm from '../components/ProfessionalSummaryForm'
 import ExperienceForm from '../components/ExperienceForm'
+import EducationForm from '../components/EducationForm'
 
 const ResumeBuilder = () => {
 
@@ -120,6 +121,11 @@ const ResumeBuilder = () => {
               { activeSection.id === 'experience' && (
                <ExperienceForm data={resumeData.experience}
                onChange={(data)=> setResumeData(prev=>({...prev, experience:data}))} />
+              )}
+
+               { activeSection.id === 'education' && (
+               <EducationForm data={resumeData.education}
+               onChange={(data)=> setResumeData(prev=>({...prev, education:data}))} />
               )}
               
            </div>
