6fd10f26c51493ffcf7bff5c8753065e0ea94a55
spriveen <rpeanuka@gmail.com>
2026-01-22 04:40:29 -0800

    update db.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./configs/db.js";
 
 const app = express();
 const PORT =process.env.PORT || 3000;
+
+// Dtabase Connection 
+await connectDB()
 
 app.use(express.json())
 app.use(cors())
