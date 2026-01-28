b16bbc4e8bfd46c5f68157eea9b3a11d8c1480b6
spriveen <rpeanuka@gmail.com>
2026-01-28 00:05:58 -0800

    resume controller.jsx

---
+++ server/controllers/resumeController.js
@@ -0,0 +1,97 @@
+import Resume from "../models/Resume";
+
+// controller for creating resume
+
+
+
+// POST: /api/resumes/create
+export const  createResume = async (req, res) =>{
+    try {
+       const userId = req.userId;
+         const {title} = req.body;
+
+        //  create new resume
+        const newResume = await Resume.create({userId, title})
+        // return sucess messsage
+        return res.status(201).json({message: 'Resume created  successfully',
+        resume: newResume})
+    } catch (error) {
+          return res.status(400).json({message: error.message}) 
+    }
+}
+
+// controller for deleting a resume
+// DELETE: /api/resumes/delete
+export const  deleteResume = async (req, res) =>{
+    try {
+       const userId = req.userId;
+         const {resumeId} = req.params;
+
+        await Resume.findOneAndDelete({userId, _id: resumeId})
+
+        // return sucess message
+        return res.status(200).json({message: 'Resume deleted  successfully'})
+    } catch (error) {
+          return res.status(400).json({message: error.message}) 
+    }
+}
+
+// get user resume by id
+// GET: /api/resume/get
+export const  getResumeById = async (req, res) =>{
+    try {
+       const userId = req.userId;
+         const {resumeId} = req.params;
+
+         const resume = await Resume.findOne({userId, _id: resumeId})
+         if(!resume){
+            return res.status(404).json({message: "Resume not found"}) 
+         }
+
+        resume.__v = undefined;
+        resume.createdAt = undefined;
+        resume.updatedAt = undefined;
+        
+        return res.status(200).json({resume})
+    } catch (error) {
+    return res.status(400).json({message: error.message}) 
+    }
+}
+
+// get resume by id public
+// GET: /api/resumes/public
+
+export const getPublicResumeById = async (req, res) =>{
+   try {
+    const {resumeId} = req.params;
+    const resume = await Resume.findOne({public: true, _id: resumeId})
+
+    if(!resume){
+            return res.status(404).json({message: "Resume not found"}) 
+         }
+    
+    return res.status(200).json({resume})
+   } catch (error) {
+    return res.status(400).json({message: error.message})
+   }
+}
+
+// controller for updating a resume
+// PUT: /api/resume/update
+ export const updateResume = async (req,res) =>{
+    try {
+       const userId  = req.userId;
+       const {resumeId, resumeData, removebackground} = req.body
+       const image = req.file;
+
+       let resumeDataCopy = JSON.parse(resumeData);
+
+     const resume = await  Resume.findByIdAndUpdate({userId, _id:resumeId}, resumeDataCopy,
+        {new: true})
+
+        return res.status(200).json({message: 'Saved sucessfully', resume})
+
+    } catch (error) {
+        return res.status(400).json({message:error.message})
+    }
+ }
