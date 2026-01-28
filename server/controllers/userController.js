a952a295971dbc77ef5ab9e78d9ab01d95f42997
spriveen <rpeanuka@gmail.com>
2026-01-27 21:47:17 -0800

    update usercontrollers.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -43,3 +43,54 @@ export const  registerUser = async (req, res) => {
         return res.status(400).json({message: error.message})
     }
 }
+
+//  controller for user login
+// post :/api/user/login
+export const  loginUser = async (req, res) => {
+    try {
+        const { email, password} = req.body;
+
+       
+        //   check if user  exisits
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message:'Invalid email or password'})
+        }
+      //   check if password is correct  
+      if(!user.comparePaasword(password)){
+         return res.status(400).json({message:'Invalid email or password'})
+      }
+
+    //   return success message
+const token = generateToken(user._id)
+    user.password = undefined;
+
+    return res.status(200).json({message:'Login sucessfully', token, user})
+
+     } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
+
+// controller for getting userby id
+// GET: /api/user/data
+
+export const  getUserById = async (req, res) => {
+    try {
+        
+      const userId = req.userId;
+
+    //   check if user exists
+  const user = await User.findById(userId)
+  if(!user){
+    return res.status(404).json({message: "User not found"})
+  }
+
+   //   return user
+   user.password = undefined;
+return res.status(200).json( {user})
+
+     } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
