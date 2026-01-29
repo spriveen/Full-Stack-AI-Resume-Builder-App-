00dd493d705cbadc3cbb004aa6f919bb7c52d5b8
spriveen <rpeanuka@gmail.com>
2026-01-29 06:44:02 -0800

    update loading components

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,3 +1,8 @@
+import {configureStore} from '@reduxjs/toolkit'
+import authReducer from './features/authSlice'
 
-
-export const store = 
+export const store = configureStore({
+    reducer : {
+        auth: authReducer
+    }
+})
